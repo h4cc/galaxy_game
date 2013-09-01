46b68b756a0188d1297c5dc7d1ade5bc233cbf8e
Julius Beckmann <github@h4cc.de>
2013-09-01 19:49:32 +0200

    Implemented everything while on my wait to the airport. All Tests run. Commiting from noisy airport.

--- src/galaxy_game.erl
+++ src/galaxy_game.erl
@@ -28,6 +28,12 @@
 
 -export([setup_universe/3, teardown_universe/1, simulate_attack/2]).
 
+% State for the planet proc.
+% The planet knows its own name so far.
+-record(planet, {
+  name::planet()
+}).
+
 %% @doc Set up a universe described by the input.
 %% The imput is asumed to be minimal and non redundant (i.e. if there is an
 %% alliance {a, b} there won't be an alliance {b, a}).
@@ -36,7 +42,35 @@
 -spec setup_universe([planet()], [shield()], [alliance()]) -> ok.
 %% @end
 setup_universe(Planets, Shields, Alliances) ->
-    unimplemented.
+    % Create planets
+    lists:foreach( 
+      fun(Name) ->
+          Pid = spawn(fun() -> 
+              IsShielded = lists:member(Name, Shields),
+              process_flag(trap_exit, IsShielded),
+              planet_proc(#planet{name = Name}) 
+          end),
+          register(Name, Pid),
+          % Check that planet exists with a ping pong
+          Pid ! {self(), ping},
+          receive
+            pong -> ok
+          end
+      end, 
+      Planets
+    ),
+    % Create alliances
+    lists:foreach( 
+      fun({Partner1, Partner2}) ->
+          Partner1 ! {self(), link_to, whereis(Partner2)},
+          % Wait for ack.
+          receive 
+            ack -> ok
+          end
+      end, 
+      Alliances
+    ),
+    ok.
 
 %% @doc Clean up a universe simulation.
 %% This function will only be called after calling setup_universe/3 with the
@@ -46,7 +80,22 @@ setup_universe(Planets, Shields, Alliances) ->
 -spec teardown_universe([planet()]) -> ok.
 %% @end
 teardown_universe(Planets) ->
-    unimplemented.
+    % Shutdown planets
+    lists:foreach( 
+      fun(Name) ->
+          case whereis(Name) of
+              % Planet does not exist.
+              undefined ->
+                  ok;
+              % Tear planet down.
+              Pid -> 
+                  Pid ! teardown,
+                  unregister(Name)
+          end
+      end, 
+      Planets
+    ),
+    ok.
 
 %% @doc Simulate an attack.
 %% This function will only be called after setting up a universe with the same
@@ -55,5 +104,58 @@ teardown_universe(Planets) ->
 -spec simulate_attack([planet()], [attack()]) -> Survivors::[planet()].
 %% @end
 simulate_attack(Planets, Actions) ->
-    unimplemented.
+    % Send attacks.
+    lists:foreach(
+        fun({Type, Planet}) ->
+            case Type of
+                % A nuclear attack will simply destroy that planet.
+                nuclear ->
+                    exit(whereis(Planet), kill);
+                % Send a special laser attack, that only shielded planets will survive.
+                laser ->
+                    exit(whereis(Planet), laser)   
+            end
+        end,
+        Actions
+    ),
+    % Need to wait a short time to have the kills be evaluated.
+    timer:sleep(1),
+    % Filter planet processes that are still alive.
+    Survivors = lists:filter(
+        fun(Planet) ->
+            PPid = whereis(Planet),
+            PPid /= undefined andalso erlang:is_process_alive(PPid)
+        end,
+        Planets
+    ),
+    Survivors.
 
+%--- Implementation ---
+
+% @doc Process function for a planet.
+-spec planet_proc(#planet{}) -> ok.
+planet_proc(#planet{name = Name} = Planet) ->
+    receive 
+        % Offer ping pong.
+        {From, ping} ->
+            From ! pong,
+            planet_proc(Planet);
+        % Will stop the server without usage of exit().
+        teardown ->
+            ok;
+        % Synchronous command to link this process to another.
+        {FromPid, link_to, Pid} ->
+            link(Pid),
+            FromPid ! ack,
+            planet_proc(Planet);
+        % If this process  get a killed message, it is shielded, so simply ignore it.
+        {'EXIT', _RemotePid, killed} ->
+            planet_proc(Planet);
+        % If this process  get a laser message, it is shielded, so simply ignore it.
+        {'EXIT', _RemotePid, laser} ->
+            planet_proc(Planet);
+        % Uhh, errors! Why?
+        UnknownMsg ->
+            io:format("Planet ~p: Unknown Message recieved: ~p~n", [Name, UnknownMsg]),
+            planet_proc(Planet)
+    end.
